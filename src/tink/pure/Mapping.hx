369d591bfa9c92b3a76f5cb535d1b368a8af818a
back2dos <back2dos@gmail.com>
2017-03-30 11:53:53 +0200

    Fix condensing and expose it.

--- src/tink/pure/Mapping.hx
+++ src/tink/pure/Mapping.hx
@@ -1,5 +1,7 @@
 package tink.pure;
 
+using tink.CoreApi;
+
 @:structInit private class MapEntry<K, V> {
   public var key(default, never):K;
   public var isset(default, never):Bool;
@@ -12,16 +14,20 @@ package tink.pure;
   public inline function new() this = null;
   
   public function exists(key:K) {
+
     for (p in this)
-      if (p.key == key) return p.isset;
-      else if (p.condensed != null) return p.condensed.exists(key);
+      if (p.condensed != null) return p.condensed.exists(key);
+      else if (p.key == key) return p.isset;
+    
     return false;
   }
   
   @:arrayAccess public function get(key:K) {
+
     for (p in this)
-      if (p.key == key) return p.value;
-      else if (p.condensed != null) return p.condensed.get(key);
+      if (p.condensed != null) return p.condensed.get(key);
+      else if (p.key == key) return p.value;
+
     return null;
   }
   
@@ -31,17 +37,25 @@ package tink.pure;
   public function with(key:K, value:V):Mapping<K, V> 
     return this.prepend({ key: key, isset: true, value: value, condensed: null });
 
+  @:extern inline function alloc():Map<K, V> return new Map();
+
   //Everything beyond this point is for the brave
   @:extern inline public function iterator()
-    return toMutable().iterator();
+    return getCondensed().or(alloc).iterator();
 
   @:extern inline public function keys()
-    return toMutable().keys();
+    return getCondensed().or(alloc).keys();
 
-  @:to @:extern inline function toMutable():Map<K, V>
+  @:to @:extern inline public function condensed():Mapping<K, V>
+    return switch getCondensed() {
+      case None: null;
+      case Some(v): v;
+    }
+
+  @:to @:extern inline function getCondensed() {
     return switch this.first() {
-      case None: new Map();
-      case Some({ condensed: c }) if (c != null): c;
+      case None: None;
+      case Some({ condensed: c }) if (c != null): Some(c);
       case Some(first):
 
         var ret = new Map<K, V>();
@@ -55,24 +69,27 @@ package tink.pure;
                 else excluded[entry.key] = true;
               }
             case c:
-              for (k in c.keys()) if (!excluded.exists(k)) ret.set(k, c[k]);
+              for (k in c.keys()) if (!excluded.exists(k) && !ret.exists(k)) ret.set(k, c[k]);
               break;
           }
 
-        return first.condensed = ret;
+        return Some(first.condensed = ret);
     }
+  }
+
+  @:to @:extern inline function toMutable():Map<K, V>
+    return getCondensed().map(function (m) return merge([m])).or(alloc);
 
   @:extern inline static function merge<K, V>(a:Array<Map<K, V>>) 
     return [for (m in a) for (k in m.keys()) k => m[k]];
 
   @:from @:extern inline static function ofMutable<K, V>(v:Map<K, V>):Mapping<K, V> {
     var ret = new List<MapEntry<K, V>>();
-    var first = true;
-
-    for (k in v.keys())
-      ret = ret.prepend({ key: k, isset: true, value: v[k], condensed: if (first == true) { first = false; merge([v]); } else null });
     
-    return ret;
+    return
+      if (v.iterator().hasNext()) 
+        ret.prepend({ key: null, isset: false, value: null, condensed: merge([v]) });
+      else ret;
   }
 
   @:op(a + b) @:extern inline static function rAddMutable<K, V>(m:Mapping<K, V>, other:Map<K, V>):Mapping<K, V> 
