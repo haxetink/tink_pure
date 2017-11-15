a1cc909dfe04667676a66997cfef7c0059fa76c8
Juraj Kirchheim <back2dos@googlemail.com>
2017-11-15 17:53:09 +0100

    A few more functions exposed with some tests.

--- src/tink/pure/Slice.hx
+++ src/tink/pure/Slice.hx
@@ -8,16 +8,25 @@ abstract Slice<T>(MSlice<T>) to MSlice<T> {
   inline function new(v:MSlice<T>) 
     this = v;
 
+  public inline function skip(count):Slice<T> 
+    return new Slice(this.skip(count));
+
+  public inline function limit(count):Slice<T>     
+    return new Slice(this.limit(count));
+
+  public inline function reverse():Slice<T>
+    return new Slice(this.reverse());
+
   @:arrayAccess inline function get(index:Int):T
     return this[index];
 
   static function make<T>(v:MSlice<T>)
     return new Slice(if (v.isShared || v.getOverhead() > 1.0) v.compact() else v);
 
-  @:from static inline function ofArray<T>(a:Array<T>)
+  @:from static public inline function ofArray<T>(a:Array<T>)
     return make(a);
 
-  @:from static inline function ofVector<T>(v:haxe.ds.Vector<T>)
+  @:from static public inline function ofVector<T>(v:haxe.ds.Vector<T>)
     return make(v);
 
 }
--- tests/SliceTest.hx
+++ tests/SliceTest.hx
@@ -6,9 +6,12 @@ class SliceTest {
   public function basic() {
     var a = [for (i in 0...100) i];
     var s:Slice<Int> = a;
+    var rev = s.reverse();
     asserts.assert(s.length == a.length);
-    for (i in 0...a.length)
+    for (i in 0...a.length) {
       asserts.assert(s[i] == a[i]);
+      asserts.assert(rev[a.length - i - 1] == a[i]);
+    }
     return asserts.done();
   }
 }
